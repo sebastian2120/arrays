300a0572f2f4f17b0996ddfb2d1f053b1d264ca9
sebastian2120 <61993181+sebastian2120@users.noreply.github.com>
2020-04-25 09:52:00 -0500

    Add files via upload

---
+++ conjuntos.php
@@ -0,0 +1,89 @@
+<?php
+$pueblos = array(
+"San Francisco Coacalco.","San Lorenzo Tetlixtac.","Santa María Magdalena Huizachitla."
+);
+echo("<br>Pueblos Originarios:<br>");
+ foreach ($pueblos as $key => $value) {
+   echo("<br>".$value);
+ }
+
+
+
+ $colonias = array(
+"Ampliacion Villa de Reyes. ","Ejidal Canuto Luna.","El Gigante. ","El Granero.","Hidalgo.","Loma Bonita.","Los Acuales","Potrero Popular.","Potrero Popular II.","República Mexicana.","San José.","Zacuautitla."
+);
+echo("<br><br><br> Colonias:<br>");
+ foreach ($colonias as $key => $value) {
+    echo("<br>".$value);
+  }
+
+
+
+
+  $fracciones = array(
+ "Bonito La Loma.","Camino Real El Granero.","El Pantano.","Granjas de San Cristóbal.","Jardines de San José.","Las Garzas.","Lomas de Coacalco.","Parque Residencial Coacalco Secciones 1ª, 2ª Y 3ª","Unidad Coacalco Villa de las Flores, Prolongación 1ª sección, 1ª sección C, T y Jardín/Las plazas, Las Villas.","Villas Gigante, Allende y Las Villas."
+);
+ echo("<br><br><br> Fraccionamientos:<br>");
+  foreach ($fracciones as $key => $value) {
+     echo("<br>".$value);
+   }
+
+
+
+   $condominios = array(
+     "Ex Rancho San Felipe.", "Habitacional Asociación de Comerciantes de Coacalco.", "Habitacional Bonito Coacalco.", "Habitacional Bonito La Cruz 5. Habitacional Colonial Coacalco.", "Habitacional Columnas Coacalco.", "Habitacional Conjunto Salamanca.", "Habitacional COSYD.", "Habitacional El Chaparral.", "Habitacional El Laurel.", "Habitacional El Potrero II.", "Habitacional El Vergel.", "Habitacional Fuentes de San Francisco.", "Habitacional Hacienda Cruztitla.", "Habitacional Hacienda El Teruel.", "Habitacional Hacienda San Pablo.", "Habitacional Hacienda Taxco Viejo.", "Habitacional Joyas Coacalco.", "Habitacional La Cualac.", "Habitacional La Guadalupana.", "Habitacional La Mohonera Star II."
+   );
+  echo("<br><br><br> Condominios:<br>");
+   foreach ($condominios as $key => $value) {
+      echo("<br>".$value);
+    }
+
+
+    $condominios1 = array(
+      "Habitacional La Providencia.", "Habitacional Las Brisas.", "Habitacional Las Hiedras.", "Habitacional Lilas.", "Habitacional Los Almendros.","Habitacional Los Olivos.","Habitacional Los Portales Oriente.","Habitacional Los Portales Poniente.","Habitacional Mediterráneo.", "Habitacional Niños Héroes."," Habitacional Parque Taxco Viejo."," Habitacional Plaza Las Flores."," Habitacional Plaza Los Arcos."," Habitacional Portón Taxco Viejo."," Habitacional Potrero I.","Habitacional Privadas de Coacalco."," Habitacional Rancho Jalatlaco.","Habitacional Real de Coacalco."," Habitacional San Diego."," Habitacional SYTATIR."," Habitacional Tetlacolili."," Habitacional Valle Florido."," Habitacional Villa de Reyes."," Habitacional Villa Florencia."," Habitacional Villa Florida."
+    );
+    foreach ($condominios1 as $key => $value) {
+       echo("<br>".$value);
+     }
+
+
+     $condominios2 = array(
+      "Habitacional Villa Las Manzanas."," Parque Industrial ZI-1 y ZI-2.","Residencial Cedros 1 y 2."," Residencial Hacienda Capultitla."," Residencial La Cima."," Residencial La Floresta."," Residencial Las Dalias I, II, III y IV."," Residencial Los Sabinos I y II."," Residencial Paraíso."," Residencial Plaza Coacalco.","Residencial Santander."," Residencial Sira Park.","Rincón Coahuilense."," Rincón de las Fuentes."," Rinconada Coacalco."," Rinconada San Lorenzo."," San Francisco."," Zer-K (San Salvador)."," Quintas Coacalco."," Habitacional Tulipán.","Habitacional Rinconada Boulevares."," Habitacional Quinta Boulevares.","Habitacional Los Abetos (Villas Buitrón).","Habitacional Las Rosas."
+       );
+     foreach ($condominios2 as $key => $value) {
+        echo("<br>".$value);
+      }
+
+
+
+
+      $conjuntos = array(
+      "Arte y Publicidad Jorge Briseño."," Bosques del Valle 1ª y 2ª Sección."," Calpulli del Valle."," El Laurel."," El Oasis."," El Obelisco. "," Ex Hacienda San Felipe."," Hacienda Las Garzas."," Jalatlaco."," La Vista."," Las Bugambilias."," Las Dalias."," Lomas de Coacalco Sección Bosques de Coacalco."," Los Periodistas Revolucionarios."," Privadas Las Garzas."," Rancho la Palma."," Rancho La Providencia."," Rinconada San Felipe I."," Rinconada San Felipe II."," San Francisco Coacalco Sección Héroes y Sección Hacienda."," Santa María I."," Santa María II."," Santa María III."," Cosmopol."," Residenza Privadas."
+      );
+     echo("<br><br><br> Conjuntos Urbanos:<br>");
+      foreach ($conjuntos as $key => $value) {
+         echo("<br>".$value);
+       }
+
+
+
+
+       $unidades = array(
+       "Coor Coacalco."," Infonavit Granjas Cor Norte y Sur."," José María Morelos y Pavón 1ª Sección."," Potrero La Laguna."," San Rafael Coacalco."
+       );
+      echo("<br><br><br> Unidades Habitacionales:<br>");
+       foreach ($unidades as $key => $value) {
+          echo("<br>".$value);
+        }
+
+
+
+
+        $cultivo = array(
+        "Ex Ejido de Coacalco."
+        );
+       echo("<br><br><br> Zona de cultivo:<br>");
+        foreach ($cultivo as $key => $value) {
+           echo("<br>".$value);
+         }
+?>
